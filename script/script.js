724a9f643d8ec40216a2e841ccb6112a10d59bc5
Sebabatso Mashego <sebabatsomashego.dev@gmail.com>
2021-05-21 18:27:44 +0200

    Updated
    added styling for portfolio section and edited html code for buttons

--- script/script.js
+++ script/script.js
@@ -4,16 +4,18 @@ const close_btn = document.querySelector(".close-btn");
 const overlay = document.querySelector(".overlay");
 const menu = document.querySelector(".menu");
 const menu_options = document.querySelectorAll(".navigation__link");
+const scroll_progress_bar = document.querySelector(".scroll-progress");
+const container_head = document.querySelector(".container-head");
 
 open_btn.addEventListener("click", () => {
     close_btn.classList.remove("hidden");
     overlay.classList.remove("hidden");
     overlay.classList.add("opened");
     menu.classList.remove("hidden");
-    body.classList.add('no-scroll');
+    body.classList.add("no-scroll");
 
     menu_options.forEach((option) => {
-        option.classList.add('opened');
+        option.classList.add("opened");
     });
 });
 
@@ -28,3 +30,16 @@ close_btn.addEventListener("click", () => {
         option.classList.remove("opened");
     });
 });
+
+function scrolled() {
+    let scroll = document.documentElement.scrollTop;
+    let height = container_head.clientHeight;
+
+    let scrolled = (scroll / height) * 100;
+
+    scroll_progress_bar.style.width = scrolled + "%";
+}
+
+window.onscroll = () => {
+    scrolled();
+};
